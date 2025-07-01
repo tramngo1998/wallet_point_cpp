f7f51f74888acf764184ebdc59f44d21bc5e8da5
tram.ngo <tram.ngo@sgh.asia>
2025-07-01 21:25:34 +0700

    Tạo class WalletManager và mở kết nối đến CSDL SQLite

---
+++ app/walletManager.cpp
@@ -0,0 +1,12 @@
+﻿#include "walletManager.h"
+
+WalletManager::WalletManager() {
+    if (sqlite3_open("data/users.db", &db) != SQLITE_OK) {
+        cerr << "Khong the ket noi SQLite!" << endl;
+    }
+    createWalletDB();
+}
+
+WalletManager::~WalletManager() {
+}
+
---
+++ app/walletManager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef WALLET_MANAGER_H
+#define WALLET_MANAGER_H
+
+#include <iostream>
+#include <sqlite3.h>
+#include <string>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+class WalletManager {
+private:
+    sqlite3* db;
+	void createWalletDB();
+
+public:
+    WalletManager();
+    ~WalletManager();
+
+};
+
+#endif
+
